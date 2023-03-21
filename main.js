1bccb73068df7caf2e047b345e685081f1c602c4
Ericles Brum <ericlespbrum@gmail.com>
2023-03-20 22:34:25 -0300

    adjust faq

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.querySelectorAll(".collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+  coll[i].addEventListener("click", function () {
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
+      this.classList.remove('selected');
+      this.querySelector('.arrow').classList.remove('rotate');
+      content.style.margin = "0px 0px 0px 0px"
     } else {
+      this.classList.add('selected');
+      this.querySelector('.arrow').classList.add('rotate');
       content.style.maxHeight = content.scrollHeight + "px";
-    } 
+      content.style.margin = "0px 0px 20px 0px"
+    }
   });
 }
